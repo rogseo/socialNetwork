37c16f8758b13b252e6b35b210d083153c64eb20
rogseo <rogseo@gmail.com>
2022-11-20 21:27:06 -0600

    add put/delete user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,77 @@
-const User=require('../models/User');
+const User = require('../models/User');
 
-module.exports={
-    getUsers(req,res){
+module.exports = {
+    getUsers(req, res) {
         User.find()
-        .then((users)=>res.json(users))
-        .catch((err)=>res.status(500).json(err));
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
     },
-    getSingleUser(req,res){
-        User.findOne({_id:req.params.userId})
-        .then((user)=>
-        !user
-        ?res.status(404).json({message:'No user with that ID'})
-        :res.json(user)
-        )
-        .catch((err)=>res.status(500).json(err));
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
     },
-    createUser(req,res){
+    createUser(req, res) {
         User.create(req.body)
-        .then((dbUserData)=>res.json(dbUserData))
-        .catch((err)=>res.status(500).json(err));
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => res.status(500).json(err));
+    },
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with this id!' })
+                    : res.json(user)
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with that ID' })
+              : User.deleteMany({ _id: { $in: user.friends } })
+          )
+          .then(() => res.json({ message: 'User and friends deleted!' }))
+          .catch((err) => res.status(500).json(err));
+      },
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No User with this id!' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+
+    },
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No User with this id!' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+
     },
 };
--- models/User.js
+++ models/User.js
@@ -45,12 +45,7 @@ userSchema
   .get(function () {
     return this.friends.length;
   });
-  // Setter to set the first and last name
-//   .set(function (v) {
-//     const first = v.split(' ')[0];
-//     const last = v.split(' ')[1];
-//     this.set({ first, last });
-//   });
+
 
 // Initialize our User model
 const User = model('user', userSchema);
