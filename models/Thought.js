9e8929df57da0d05e143892ff32e0496243b1592
rogseo <rogseo@gmail.com>
2022-11-20 18:31:43 -0600

    add models/Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { truncateSync } = require('fs');
+const { Schema, model } = require('mongoose');
+const Reaction=require('Reaction');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [Reaction],
+
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema
+.virtual('reactionCount')
+.get(function(){
+    return this.reactions.length;
+})
+
+const Thought = model('thought', thoughtSchema);
+module.exports=Thought;
