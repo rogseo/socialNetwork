3d79641747396b566df8d283c3e0d1bf884a2720
rogseo <rogseo@gmail.com>
2022-11-20 20:12:05 -0600

    add edited thought controller object to thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,12 +3,25 @@ const {
   getThought,
   getSingleThought,
   createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
 } = require('../../controllers/thoughtController');
 
 // /api/users
 router.route('/').get(getThought).post(createThought);
 
 // /api/users/:userId
-router.route('/:thoughtId').get(getSingleThought);
+router
+.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
 
 module.exports = router;
